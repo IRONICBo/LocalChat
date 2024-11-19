f3272f56f4eab4ee75134144dc00b830cbbae9c6
Bo <boironic@gmail.com>
2024-11-19 23:02:00 +0800

    feat: change backend to ollama.

--- localchat/embedding.py
+++ localchat/embedding.py
@@ -5,7 +5,7 @@ import os
 from llama_index.embeddings import HuggingFaceEmbedding
 from llama_index import VectorStoreIndex, ServiceContext, SimpleDirectoryReader
 from llama_index.node_parser import SimpleNodeParser
-from llama_index.llms import OpenAI
+from llama_index.llms import Ollama
 from llama_index.retrievers import VectorIndexRetriever
 
 # init embedding model and reranker model
@@ -26,10 +26,10 @@ query_embedding = embed_model.get_query_embedding(query)
 passages_embeddings = embed_model.get_text_embedding_batch(passages)
 
 # example #2. rag example
-llm = OpenAI(model='gpt-3.5-turbo-0613', api_key=os.environ.get('OPENAI_API_KEY'), api_base=os.environ.get('OPENAI_BASE_URL'))
+llm = Ollama(model="qwen:0.5b")
 service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
 
-documents = SimpleDirectoryReader(input_files=["BCEmbedding/tools/eval_rag/eval_pdfs/Comp_en_llama2.pdf"]).load_data()
+documents = SimpleDirectoryReader(input_files=["1.pdf"]).load_data()
 node_parser = SimpleNodeParser.from_defaults(chunk_size=400, chunk_overlap=80)
 nodes = node_parser.get_nodes_from_documents(documents[0:36])
 index = VectorStoreIndex(nodes, service_context=service_context)
