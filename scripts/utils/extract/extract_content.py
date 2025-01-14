8d0870c8e8be7a82bf1aa3de9477ad21370d2a3d
Bo <boironic@gmail.com>
2025-01-14 23:02:00 +0800

    feat: add markdown content extractor.

--- scripts/utils/extract/extract_content.py
+++ scripts/utils/extract/extract_content.py
@@ -5,3 +5,25 @@ downloaded = fetch_url('https://github.blog/2019-03-29-leader-spotlight-erin-spi
 # output main content and comments as plain text
 result = extract(downloaded)
 print(result)
+
+markdown = extract(downloaded, output_format="markdown", with_metadata=True)
+print(markdown)
+
+# Convert local html file to markdown
+with open("demo.html", "r") as f:
+    html = f.read()
+    markdown = extract(html, output_format="markdown", with_metadata=True)
+    with open("demo.md", "w") as f:
+        f.write(markdown)
+
+with open("test_blog.html", "r") as f:
+    html = f.read()
+    markdown = extract(html, output_format="markdown", with_metadata=True)
+    with open("test_blog.md", "w") as f:
+        f.write(markdown)
+
+with open("test_wikipedia.html", "r") as f:
+    html = f.read()
+    markdown = extract(html, output_format="markdown", with_metadata=True)
+    with open("test_wikipedia.md", "w") as f:
+        f.write(markdown)
---
+++ scripts/utils/extract/test_blog.md
@@ -0,0 +1,10 @@
+---
+title: Redirecting...
+date: 2023-04-21
+---
+If you are not redirected automatically, follow this
+link to example.com
+.
+If you are not redirected automatically, follow this
+link to example.com
+.
