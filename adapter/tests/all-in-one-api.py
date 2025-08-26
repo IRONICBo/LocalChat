e51233be82c9db841db18c23211426ab41038c02
Asklv <boironic@gmail.com>
2025-08-26 18:40:00 +0800

    feat: add litellm api.
    Signed-off-by: Asklv <boironic@gmail.com>

---
+++ adapter/tests/all-in-one-api.py
@@ -0,0 +1,10 @@
+from litellm import completion
+import os
+
+# For gemini
+os.environ['GEMINI_API_KEY'] = "YOUR_API_KEY"
+response = completion(
+    model="gemini/gemini-2.0-flash",
+    messages=[{"role": "user", "content": "write code for saying hi from LiteLLM"}]
+)
+print(response)
